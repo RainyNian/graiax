7ddcff2d36e7fbc21a88fa9181537f143ea5e921
zzzzz167 <2742400566@qq.com>
2021-02-11 00:52:08 +0800

    Revert "emm"
    This reverts commit 7f39c50f447581525f84dece1b5641f4abe6389a.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
+        { to: 'blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
